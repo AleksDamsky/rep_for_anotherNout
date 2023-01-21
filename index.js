d09644c29f6d54c1f1400024b6847f2baf904fe3
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2023-01-21 22:04:34 +0300

    Now my bot can to repeat and send my message

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const TelegramApi = require('node-telegram-bot-api');
 
 const token = '5851867997:AAHHk1nuV5amA9KLNvOmIXbSy5qPhTEKJxw';
 
+let bot = new TelegramApi(token, {polling: true});
+
+bot.on('message', msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `Ты написал мне ${text}`);
+})
